a81d78bf7fd2c68a4be9866a3a2e7a8074b4c9ae
刘翔宇 <a1572760234@126.com>
2020-09-22 15:09:06 +0800

    定义视图函数

--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+# 视图,就是python函数
+# 视图函数有两个要求:
+#   1.接收请求,这个请求就是HttpRequest的类对象
+#   2.必须返回一个响应
+
+#我们期望用户访问/index/来访问视图函数
+def index(request):
+    return HttpResponse('ok')
