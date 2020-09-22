5ea51003a82fc0646efe1460d24e8b174b6dca46
刘翔宇 <a1572760234@126.com>
2020-09-22 11:18:39 +0800

    set keys name:varchar(10)

--- book/models.py
+++ book/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 # Create your models here.
+
+# 1.模型类,需要继承自models.Model
+# 2.系统会自动为该类添加主键(属性)----id
+# 3.定义name字段(属性)  字段名 = modle.类型(选项)
+# 字段名其实就是数据表的字段名,不要使用Python/Mysql的关键字
+class Book(models.Model):
+    #id已自动创建
+
+    #name,varchar(选项)===>varchar(10)
+    name = models.CharField(max_length=10)
